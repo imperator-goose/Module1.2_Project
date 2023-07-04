388008a4e9caecb2dea8c8508cfc055a8a71b082
imperator-goose <glookeemaska133@gmail.com>
2023-07-04 13:20:22 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String IncomingText = in.nextLine();
-        boolean correct_or_not_correct;
-        if (IncomingText == "{()[]}" || IncomingText == "{[]()}" || IncomingText == "[{}()]"||IncomingText == "[(){}]"||IncomingText == "[(){}]" ||IncomingText == "[({})]"||IncomingText == "[{()}]"||IncomingText == "([{}])"||IncomingText == "([{}])"||IncomingText == "([{]})"||IncomingText == "({[}])"||IncomingText == "{([)]}"||IncomingText == "{[(])}"||IncomingText == "[{(})]"||IncomingText == "[({)}]") {
-            correct_or_not_correct = true;
-        } else {
-            correct_or_not_correct = false;
+    public static Collection getCommonElements(List<Integer> collection1,List<Integer> collection2){
+        Collection<Integer> commonElements = new ArrayList<>();
+        HashSet<Integer> tempSet = new HashSet<>(collection1);
+        for (int elements : collection2){
+            if(tempSet.contains(elements)){
+                commonElements.add(elements);
+            }
         }
-        if (correct_or_not_correct = true){
-            System.out.println("Все скобки закрыты");
-        } else {
-            System.out.println("Неправильный ввод");
+        List<Integer> sortedCommonElements = new ArrayList<>(commonElements);
+        Collections.sort(sortedCommonElements);
+        return sortedCommonElements;
+    }
+    public static void main(String[] args) {
+        List<Integer> collection1 = Arrays.asList(1,-1,3);
+        List<Integer> collection2 = Arrays.asList(1,4,3,-1);
+        Collection<Integer> CommonElements = new ArrayList<>(getCommonElements(collection1,collection2));
+        System.out.println("Общие элементы:");
+        for (int element : CommonElements){
+            System.out.println(element);
         }
     }
 }
